754c15c36422986099363afe42404b05cb036aeb
imkoi <sviridalos@gmail.com>
2024-12-09 23:09:40 +0100

    disable benchmark class generation from ci cd

--- SparseInject.Tests/Utilities/DependencyClassGenerator.cs
+++ SparseInject.Tests/Utilities/DependencyClassGenerator.cs
@@ -7,6 +7,7 @@ namespace Utilities
 {
     public class DependencyClassGenerator
     {
+        [Ignore("dont run in unit test")]
         [Test]
         [TestCase(6)]
         public void GenerateDependencies(int depth)
