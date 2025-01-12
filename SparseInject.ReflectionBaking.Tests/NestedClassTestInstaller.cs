7bf065779541fbaa0e565e737397ba417e31d914
imkoi <sviridalos@gmail.com>
2025-01-12 01:44:36 +0100

    disable implicit usings

--- SparseInject.ReflectionBaking.Tests/NestedClassTestInstaller.cs
+++ SparseInject.ReflectionBaking.Tests/NestedClassTestInstaller.cs
@@ -1,26 +1,27 @@
-﻿namespace SparseInject.ReflectionBaking.Tests.NestedClass;
-
-public class NestedClassTestInstaller
+﻿namespace SparseInject.ReflectionBaking.Tests.NestedClass
 {
-    public static void Install(IScopeBuilder scopeBuilder)
+    public class NestedClassTestInstaller
     {
-        scopeBuilder.Register<TransientDependencyA>();
-        scopeBuilder.Register<ITransientDependencyB, TransientDependencyB>();
-        scopeBuilder.Register<ITransientDependencyC0, ITransientDependencyC1, TransientDependencyC>();
-        scopeBuilder.Register<ITransientDependencyD0, ITransientDependencyD1, ITransientDependencyD2, TransientDependencyD>();
-    }
+        public static void Install(IScopeBuilder scopeBuilder)
+        {
+            scopeBuilder.Register<TransientDependencyA>();
+            scopeBuilder.Register<ITransientDependencyB, TransientDependencyB>();
+            scopeBuilder.Register<ITransientDependencyC0, ITransientDependencyC1, TransientDependencyC>();
+            scopeBuilder.Register<ITransientDependencyD0, ITransientDependencyD1, ITransientDependencyD2, TransientDependencyD>();
+        }
     
-    public class TransientDependencyA { }
+        public class TransientDependencyA { }
 
-    public interface ITransientDependencyB { }
-    public class TransientDependencyB : ITransientDependencyB { }
+        public interface ITransientDependencyB { }
+        public class TransientDependencyB : ITransientDependencyB { }
 
-    public interface ITransientDependencyC0 { }
-    public interface ITransientDependencyC1 { }
-    public class TransientDependencyC : ITransientDependencyC0, ITransientDependencyC1 { }
+        public interface ITransientDependencyC0 { }
+        public interface ITransientDependencyC1 { }
+        public class TransientDependencyC : ITransientDependencyC0, ITransientDependencyC1 { }
 
-    public interface ITransientDependencyD0 { }
-    public interface ITransientDependencyD1 { }
-    public interface ITransientDependencyD2 { }
-    public class TransientDependencyD : ITransientDependencyD0, ITransientDependencyD1, ITransientDependencyD2 { }
+        public interface ITransientDependencyD0 { }
+        public interface ITransientDependencyD1 { }
+        public interface ITransientDependencyD2 { }
+        public class TransientDependencyD : ITransientDependencyD0, ITransientDependencyD1, ITransientDependencyD2 { }
+    }
 }
--- SparseInject.ReflectionBaking.Tests/ScopeTestInstaller.cs
+++ SparseInject.ReflectionBaking.Tests/ScopeTestInstaller.cs
@@ -1,66 +1,69 @@
-﻿namespace SparseInject.ReflectionBaking.Tests.Scope;
+﻿using System;
 
-public static class ScopeTestInstaller
+namespace SparseInject.ReflectionBaking.Tests.Scope
 {
-    public static void Install(IScopeBuilder scopeBuilder)
+    public static class ScopeTestInstaller
     {
-        scopeBuilder.Register<Dependency>();
-        
-        scopeBuilder.RegisterScope<ScopeA>(scopeA =>
+        public static void Install(IScopeBuilder scopeBuilder)
         {
-            scopeA.Register<DependencyA>();
-            scopeA.Register<DependencyA>();
-            scopeA.Register<DependencyA>();
-        });
+            scopeBuilder.Register<Dependency>();
         
-        scopeBuilder.RegisterScope<IScopeB, ScopeB>(scopeB =>
-        {
-            scopeB.Register<DependencyB>();
-            scopeB.Register<DependencyB>();
-            scopeB.Register<DependencyB>();
-        });
+            scopeBuilder.RegisterScope<ScopeA>(scopeA =>
+            {
+                scopeA.Register<DependencyA>();
+                scopeA.Register<DependencyA>();
+                scopeA.Register<DependencyA>();
+            });
         
-        scopeBuilder.RegisterScope<ScopeC>((scopeC, parentScopeC) =>
-        {
-            scopeC.Register<DependencyC>();
-            scopeC.Register<DependencyC>();
-            scopeC.Register<DependencyC>();
-        });
+            scopeBuilder.RegisterScope<IScopeB, ScopeB>(scopeB =>
+            {
+                scopeB.Register<DependencyB>();
+                scopeB.Register<DependencyB>();
+                scopeB.Register<DependencyB>();
+            });
         
-        scopeBuilder.RegisterScope<IScopeD, ScopeD>((scopeD, parentScopeD) =>
-        {
-            scopeD.Register<DependencyD>();
-            scopeD.Register<DependencyD>();
-            scopeD.Register<DependencyD>();
-        });
+            scopeBuilder.RegisterScope<ScopeC>((scopeC, parentScopeC) =>
+            {
+                scopeC.Register<DependencyC>();
+                scopeC.Register<DependencyC>();
+                scopeC.Register<DependencyC>();
+            });
+        
+            scopeBuilder.RegisterScope<IScopeD, ScopeD>((scopeD, parentScopeD) =>
+            {
+                scopeD.Register<DependencyD>();
+                scopeD.Register<DependencyD>();
+                scopeD.Register<DependencyD>();
+            });
+        }
     }
+
+    public interface IScopeB : IDisposable { }
+    public interface IScopeD : IDisposable { }
+
+    public class ScopeA : SparseInject.Scope
+    {
+        public ScopeA(Dependency[] dependencies, DependencyA[] dependenciesA) { }
+    }
+
+    public class ScopeB : SparseInject.Scope, IScopeB
+    {
+        public ScopeB(Dependency[] dependencies, DependencyB[] dependenciesB) { }
+    }
+
+    public class ScopeC : SparseInject.Scope
+    {
+        public ScopeC(Dependency[] dependencies, DependencyC[] dependenciesC) { }
+    }
+
+    public class ScopeD : SparseInject.Scope, IScopeD
+    {
+        public ScopeD(Dependency[] dependencies, DependencyD[] dependenciesD) { }
+    }
+
+    public class Dependency { }
+    public class DependencyA { }
+    public class DependencyB { }
+    public class DependencyC { }
+    public class DependencyD { }
 }
-
-public interface IScopeB : IDisposable { }
-public interface IScopeD : IDisposable { }
-
-public class ScopeA : SparseInject.Scope
-{
-    public ScopeA(Dependency[] dependencies, DependencyA[] dependenciesA) { }
-}
-
-public class ScopeB : SparseInject.Scope, IScopeB
-{
-    public ScopeB(Dependency[] dependencies, DependencyB[] dependenciesB) { }
-}
-
-public class ScopeC : SparseInject.Scope
-{
-    public ScopeC(Dependency[] dependencies, DependencyC[] dependenciesC) { }
-}
-
-public class ScopeD : SparseInject.Scope, IScopeD
-{
-    public ScopeD(Dependency[] dependencies, DependencyD[] dependenciesD) { }
-}
-
-public class Dependency { }
-public class DependencyA { }
-public class DependencyB { }
-public class DependencyC { }
-public class DependencyD { }
