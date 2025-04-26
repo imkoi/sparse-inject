dac445d4bbeaba7da88b52b64f91286ae2a75a81
artemsv <sviridalos@gmail.com>
2025-04-26 21:13:14 +0200

    fixed argument out of range exception in circular dependency validator

---
+++ SparseInject.Tests/Bugfixes/Bug_7.cs
@@ -0,0 +1,45 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SparseInject.Tests.Bugfixes
+{
+    public class Bug_7
+    {
+        interface IGlobalService {}
+        class GlobalService : IGlobalService {}
+
+        class SampleScope : Scope {}
+
+        interface IScopedService {}
+        class ScopedService : IScopedService {}
+
+        class ScopedServiceWithDependencies
+        {
+            public ScopedServiceWithDependencies(IGlobalService globalService, IScopedService scopedService)
+            {
+                
+            }
+        }
+        
+        [Test]
+        public void Test()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.Register<IGlobalService, GlobalService>();
+            containerBuilder.RegisterScope<SampleScope>(sampleScopeBuilder =>
+            {
+                sampleScopeBuilder.Register<IScopedService, ScopedService>(Lifetime.Singleton);
+                sampleScopeBuilder.Register<ScopedServiceWithDependencies>(Lifetime.Singleton);
+            });
+
+            var container = containerBuilder.Build();
+
+            var graph = container.GetGraph();
+            
+            var globalScope = container.Resolve<SampleScope>();
+
+            globalScope.Should().NotBeNull();
+        }
+    }
+}
